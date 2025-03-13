865dfda1f6603b4c76d3d3978b7cd0ba53c2be79
jasxyke01 <jaspher.cortez@growthops.asia>
2025-03-13 19:56:32 +0800

    changed to relative path

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,7 @@
     <title>Simple Task Management Application</title>
 
     {{-- CSS --}}
-    @vite('https://simple-task-app-iota.vercel.app/resources/css/app.css')
+    @vite('/resources/css/app.css')
     {{-- <link rel="stylesheet" href="https://simple-task-app-git-main-jasxykes-projects.vercel.app/resources/css/app.css" /> --}}
     {{-- jQuery --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
